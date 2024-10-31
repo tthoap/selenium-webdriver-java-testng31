843075e3d57831a1837dfa8670fd0434c51d9c72
Hoa Tran <tthoaptit@gmail.com>
2024-10-31 22:06:18 +0700

    Topic_09_Default_DropDown

--- selenium/selenium/Topic_09_Default_DropDown.java
+++ selenium/selenium/Topic_09_Default_DropDown.java
@@ -1,14 +1,16 @@
 package selenium;
 
-import graphql.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -16,6 +18,7 @@ public class Topic_09_Default_DropDown {
     WebDriver driver;
     String firstName ="Kevin", lastName = "Laming", emailAddress = getEmailAddress()
             , companyName = "ABC Inc.", password = "123456";
+    String day = "31", month = "October", year = "2024";
 
     @BeforeClass
     public void beforeClass() {
@@ -31,11 +34,11 @@ public class Topic_09_Default_DropDown {
         driver.findElement(By.cssSelector("input#LastName")).sendKeys(lastName);
 
         Select day = new Select(driver.findElement(By.name("DateOfBirthDay")));
-        day.selectByVisibleText("31");
+        day.selectByVisibleText(this.day);
 
-        new Select(driver.findElement(By.name("DateOfBirthMonth"))).selectByVisibleText("October");
+        new Select(driver.findElement(By.name("DateOfBirthMonth"))).selectByVisibleText(month);
 
-        new Select(driver.findElement(By.name("DateOfBirthYear"))).selectByVisibleText("2024");
+        new Select(driver.findElement(By.name("DateOfBirthYear"))).selectByVisibleText(year);
 
         driver.findElement(By.cssSelector("input#Email")).sendKeys(emailAddress);
         driver.findElement(By.cssSelector("input#Company")).sendKeys(companyName);
@@ -47,19 +50,41 @@ public class Topic_09_Default_DropDown {
         //Verify fropdown nay la single
         Assert.assertFalse(day.isMultiple());
 
-
         //Verify so luongw trong dropdown nay la 32
+        //Assert.assertEquals(day.getOptions().size(),32);
 
+        driver.findElement(By.cssSelector("button#register-button")).click();
+        SleepInSeconds(3);
+        Assert.assertEquals(driver.findElement(By.cssSelector("div.result")).getText(), "Your registration completed");
+        driver.findElement(By.cssSelector("a.ico-logout")).click();
 
     }
 
     @Test
     public void TC_02_Login() {
         //Login
+        driver.get("https://demo.nopcommerce.com/register");
+        driver.findElement(By.cssSelector("a.ico-login")).click();
+        driver.findElement(By.cssSelector("input#Email")).sendKeys(emailAddress);
+        driver.findElement(By.cssSelector("input#Password")).sendKeys(password);
+        driver.findElement(By.cssSelector("button.login-button")).click();
+        SleepInSeconds(3);
 
         //Verify
+        driver.findElement(By.cssSelector("a.ico-account")).click();
+        SleepInSeconds(3);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("input#FirstName")).getAttribute("value"),firstName);
+        Assert.assertEquals(driver.findElement(By.cssSelector("input#LastName")).getAttribute("value"),lastName);
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthDay"))).getFirstSelectedOption().getText(), day);
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthMonth"))).getFirstSelectedOption().getText(), month);
+        Assert.assertEquals(new Select(driver.findElement(By.name("DateOfBirthYear"))).getFirstSelectedOption().getText(), year);
+
+        Assert.assertEquals(driver.findElement(By.cssSelector("input#Email")).getAttribute("value"),emailAddress);
+
 
     }
+
 
 
     @AfterClass
