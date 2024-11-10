2cd72527552f26fe8fc168e5857f08fe0920a829
Hoa Tran <tthoaptit@gmail.com>
2024-11-11 06:49:21 +0700

    Topic_15_Popup_02

--- selenium/selenium/Topic_15_Popup_02.java
+++ selenium/selenium/Topic_15_Popup_02.java
@@ -1,9 +1,10 @@
 package selenium;
 
-import graphql.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -27,7 +28,7 @@ public class Topic_15_Popup_02 {
         By newsletterPopup = By.cssSelector("div.lepopup-popup-container>div:not([style^='display:none'])");
 
         //if display => Close then go to next step(search)
-        if(driver.findElements(newsletterPopup).size() > 0 && driver.findElements(newsletterPopup).get(0).isDisplayed()) {
+        if (driver.findElements(newsletterPopup).size() > 0 && driver.findElements(newsletterPopup).get(0).isDisplayed()) {
             System.out.println("Popup is displayed");
             driver.findElement(By.cssSelector("div.lepopup-popup-container>div:not([style^='display:none']) a[onclick='return lepopup_close();']")).click();
             SleepInSeconds(2);
@@ -41,18 +42,43 @@ public class Topic_15_Popup_02 {
         SleepInSeconds(3);
         Assert.assertTrue(driver.findElement(By.xpath("//a[text()='Agile Testing Explained']")).isDisplayed());
 
-
-
-
     }
 
     @Test
-    public void TC_02_Random_Not_In_DOM() {
+    public void TC_02_Random_In_DOM() {
+        driver.get("https://vnk.edu.vn/");
+
+        //Next step: search data
+        findElement(By.xpath("//button[text()='Danh sách khóa học']")).click();
+        SleepInSeconds(3);
+        System.out.println(driver.findElement(By.cssSelector("div.title-content")).getText());
+        // Assert.assertTrue(findElement(By.cssSelector("div.title-content")).getText().contains("LỊCh Khai GiẢNg ThÁNg 11 – TẶNg BỘ Template M&E GiÚP GiẢM 50% ThỜI Gian VẼ"));
 
     }
+
+    //Step nao thao tac o man hinh Home moi dung
+
+    public WebElement findElement(By locator) {
+        if (driver.findElement(By.cssSelector("div.pum-content.popmake-content")).isDisplayed()) {
+            driver.findElement(By.cssSelector("button.pum-close.popmake-close")).click();
+            SleepInSeconds(2);
+        }
+        return driver.findElement(locator);
+
+    }
+
     @Test
     public void TC_03_Random_Not_In_DOM() {
+        driver.get("https://dehieu.vn/");
 
+        By newsletterPopup = By.cssSelector("div.modal-content.css-modal-bt");
+
+        //if display => Close then go to next step(search)
+        if (driver.findElements(newsletterPopup).size() > 0 && driver.findElements(newsletterPopup).get(0).isDisplayed()) {
+            System.out.println("Popup is displayed");
+            driver.findElement(By.cssSelector("button.close")).click();
+
+        }
     }
 
 
