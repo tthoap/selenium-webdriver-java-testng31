3e6ecc0c5fb9d1f9bd16d404ecaca852025d5448
Hoa Tran <tthoaptit@gmail.com>
2024-11-10 21:21:03 +0700

    Topic_15_Popup_02

---
+++ selenium/selenium/Topic_15_Popup_02.java
@@ -0,0 +1,72 @@
+package selenium;
+
+import graphql.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Topic_15_Popup_02 {
+    WebDriver driver;
+
+    @BeforeClass
+    public void beforeClass() {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+    }
+
+    @Test
+    public void TC_01_Random_Not_In_DOM() {
+        driver.get("https://www.javacodegeeks.com/");
+
+        SleepInSeconds(10);
+        By newsletterPopup = By.cssSelector("div.lepopup-popup-container>div:not([style^='display:none'])");
+
+        //if display => Close then go to next step(search)
+        if(driver.findElements(newsletterPopup).size() > 0 && driver.findElements(newsletterPopup).get(0).isDisplayed()) {
+            System.out.println("Popup is displayed");
+            driver.findElement(By.cssSelector("div.lepopup-popup-container>div:not([style^='display:none']) a[onclick='return lepopup_close();']")).click();
+            SleepInSeconds(2);
+        } else { //If not display => go to next step (search)
+            System.out.println("Pop is not displayed");
+        }
+
+        //Next step: search data
+        driver.findElement(By.cssSelector("input#search-input")).sendKeys("Agile Testing explained");
+        driver.findElement(By.cssSelector("button#search-submit")).click();
+        SleepInSeconds(3);
+        Assert.assertTrue(driver.findElement(By.xpath("//a[text()='Agile Testing Explained']")).isDisplayed());
+
+
+
+
+    }
+
+    @Test
+    public void TC_02_Random_Not_In_DOM() {
+
+    }
+    @Test
+    public void TC_03_Random_Not_In_DOM() {
+
+    }
+
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    public void SleepInSeconds(long timeInSecond) {
+        try {
+            Thread.sleep(timeInSecond * 1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
