43e857b7cbadbb4a976d1ca38fa2c6a2b7ba1eff
tthoap <tthoaptit@gmail.com>
2024-12-11 10:31:12 +0700

    Update Test NG part II

---
+++ selenium/testng/Topic_06_Skip.java
@@ -0,0 +1,32 @@
+package testng;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_06_Skip {
+
+    //CRUD
+    //FLow
+
+
+    @Test
+    public void shouldBeRegisterFail() {
+        System.out.println("Run TC_01");
+    }
+
+   // @Test(priority = 2) or
+
+   @Test(enable=false)
+    public void shouldBeLoginPass() {
+        System.out.println("Run TC_02");
+    }
+
+    @Test
+    public void shouldBeLoginFail() {
+        System.out.println("Run TC_03");
+    }
+}
---
+++ selenium/testng/Topic_07_Description.java
@@ -0,0 +1,33 @@
+package testng;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Topic_07_Description {
+
+// tên TC =  hàm/funtion/method của Java
+//Theo convention theo từng ngôn ngữ
+
+//Chú thích diễn giải/note
+    @Test(description="JIRA#123 -  Register should be failed") 
+    //Esclip thì show dòng chú thích này trong log, nhưng Intellij ko show
+    public void shouldBeRegisterFail() {
+        System.out.println("Run TC_01");
+    }
+
+   // @Test(priority = 2) or
+
+   @Test(enable=false)
+    public void shouldBeLoginPass() {
+        System.out.println("Run TC_02");
+    }
+
+    @Test
+    public void shouldBeLoginFail() {
+        System.out.println("Run TC_03");
+    }
+}
